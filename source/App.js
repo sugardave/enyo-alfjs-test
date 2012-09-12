7cb18accd53b7a816a2d8959bfdf56fce0c9cc66
Dave Freeman <sugardave@gmail.com>
2012-09-11 20:31:27 -0500

    rudimentary login and site list

--- source/App.js
+++ source/App.js
@@ -1,10 +1,92 @@
 enyo.kind({
 	name: "App",
+	components: [
+		{kind: "AlfWrapper", onConnect: "handleConnect", onLoadSites: "handleLoadSites"},
+		{kind: enyo.Panels, classes: "enyo-fit", components: [
+			{name: "loginPanel", kind: enyo.FittableRows, components: [
+				{kind: enyo.FittableColumns, classes: "enyo-center", components: [
+					{kind: enyo.FittableRows, components: [
+						{content: "Please log in", style: "font-size: 0.7em; font-style: italic;"},
+						{kind: onyx.InputDecorator, components: [
+							{name: "username", kind: onyx.Input, placeholder: "username", classes: "center-placeholder", onkeydown: "inputChanged"}
+						]},
+						{tag: "br"},
+						{kind: onyx.InputDecorator, components: [
+							{name: "password", kind: onyx.Input, placeholder: "password", classes: "center-placeholder", onkeydown: "inputChanged"}
+						]},
+						{tag: "br"},
+						{name: "login", kind: onyx.Button, content: "Log in", disabled: true, ontap: "connectAlf"}
+					]}
+				]},
+				{name: "sitePickerWrapper", kind: enyo.FittableColumns, classes: "enyo-center", showing: false, components: [
+					{kind: onyx.PickerDecorator, components: [
+						{content: "Choose a site"},
+						{name: "sitePicker", kind: onyx.FlyweightPicker, count: 0, onSetupItem: "setupSite", components: [
+							{name: "site", kind: "AlfNode"}
+						]}
+					]}
+				]}
+			]},
+			{name: "mainPanel", kind: enyo.FittableRows, components: [
+				{content: "Main panel"}
+			]}
+		]}
+	],
+	inputChanged: function(inSender, inEvent) {
+		setTimeout(enyo.bind(this, function() {
+			var username = this.$.username.getValue();
+			var password = this.$.password.getValue();
+			var empty = (!username || !password);
+			this.$.login.setDisabled(empty);
+		}), 0);
+	},
+	connectAlf: function() {
+		// TODO: Needs a more robust config system
+		var config = this.$.alfWrapper.getConfig();
+		var username = this.$.username.getValue();
+		var password = this.$.password.getValue();
+		config = enyo.mixin(config, {login: username, password: password});
+		this.$.alfWrapper.setConfig(config);
+		this.$.alfWrapper.connect();
+	},
+	handleConnect: function(inSender, inEvent) {
+		this.log(inEvent.error === null);
+		this.$.alfWrapper.getSites();
+
+		return true;
+	},
+	handleLoadSites: function(inSender, inEvent) {
+		if (inEvent.data) {
+			this.data = inEvent.data;
+			this.log("Count: "+this.data.length);
+			this.$.sitePickerWrapper.setShowing(this.data.length);
+			this.$.sitePicker.setCount(this.data.length);
+			//this.$.docList.setData(inEvent.data);
+		} else {
+			this.log(inEvent.error);
+		}
+
+		return true;
+	},
+	setupSite: function(inSender, inEvent) {
+		var index = inEvent.index;
+		var site = this.data[index];
+		this.log("ADDING SITE:");
+		this.log(site);
+		this.$.site.setTitle(site.shortName);
+		//this.$.sitePickerWrapper.resized();
+
+		return true;
+	}
+});
+
+enyo.kind({
+	name: "AppFoo",
 	kind: enyo.FittableRows,
 	classes: "onyx",
 	components:[
 		{kind: "AlfWrapper", onConnect: "handleConnect", onLoadSites: "handleLoadSites"},
-		{name: "docList", kind: enyo.List, onSetupItem: "handleAddItem", components: [
+		{name: "docList", kind: enyo.List, onSetupItem: "handleAddItem", ontap: "getNodes", components: [
 			{kind:"AlfNode"}
 		]}
 	],
--- source/kinds/AlfNode.js
+++ source/kinds/AlfNode.js
@@ -3,10 +3,12 @@ enyo.kind({
 	kind: onyx.Item,
 	classes: "alf-node",
 	published: {
-		title:""
+		url: "",
+		title: "",
+		description: ""
 	},
 	components: [
-		{ tag: "span", name: "title" }
+		{tag: "span", name: "title"}
 	],
 	create: function() {
 		this.inherited(arguments);
@@ -16,3 +18,19 @@ enyo.kind({
 		this.$.title.setContent(this.title);
 	}
 });
+
+
+/*"url": "\/alfresco\/service\/api\/sites\/swsdp",
+	"sitePreset": "site-dashboard",
+	"shortName": "swsdp",
+	"title": "Sample: Web Site Design Project",
+	"description": "This is a Sample Alfresco Team site.",
+	"node": "\/alfresco\/service\/api\/node\/workspace\/SpacesStore\/b4cff62a-664d-4d45-9302-98723eac1319",
+	"tagScope": "\/alfresco\/service\/api\/tagscopes\/workspace\/SpacesStore\/b4cff62a-664d-4d45-9302-98723eac1319",
+	"siteManagers":
+	[
+			"mjackson",
+			"admin"
+	],
+	"isPublic": true,
+	"visibility": "PUBLIC"*/
